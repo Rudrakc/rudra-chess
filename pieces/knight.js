a87e4f42a7673d9575c1af361a1be04aa87424d1
Rudrakc <rudrachauhan2888@gmail.com>
2024-09-19 12:26:55 +0530

    Updated Knight Class

--- pieces/knight.js
+++ pieces/knight.js
@@ -6,5 +6,24 @@ export class Knight extends Piece {
     this.type = "knight";
   }
 
-  move(newPosition) {}
+  validateMove(board, newPosition) {
+    cosnt [row, col] = boardState.getIndex(this.position);
+    const [finalRow, finalCol] = boardState.getIndex(newPosition);
+    const board = boardState.board;
+    
+    const rowOffsets = [2, 2, -2, -2, 1, 1, -1, -1];
+    const colOffsets = [1, -1, 1, -1, 2, -2, 2, -2];
+
+    for (let i = 0; i < rowOffsets.length; i++) {
+      const newRow = row + rowOffsets[i];
+      const newCol = col + colOffsets[i];
+
+      if (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+        if (newRow === finalRow && newCol === finalCol) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
