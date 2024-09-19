30668c6f0c6650f929ce0b22c0cd9c622c94213c
Rudrakc <rudrachauhan2888@gmail.com>
2024-09-19 11:28:02 +0530

    Updated Bishop class

--- pieces/bishop.js
+++ pieces/bishop.js
@@ -6,6 +6,21 @@ export class Bishop extends Piece {
         this.type = 'bishop';
     }
     
-    move(newPosition) {
+    validateMove(boardState, newPosition) {
+        cosnt [row, col] = boardState.getIndex(this.position);
+        const [finalRow, finalCol] = boardState.getIndex(newPosition);
+        const board = boardState.board;
+
+        if(Math.abs(row-finalRow) === Math.abs(col-finalCol)){
+            for(let i = row; i<finalRow; (row < finalRow) ? i++ : i--) {
+                for(let j=col ; j<finalCol; (col < finalCol) ? j++ : j--) {
+                    if(i !== row && j !== col) {
+                        if (board[i][j] !== null) return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
